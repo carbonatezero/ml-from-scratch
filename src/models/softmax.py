18676be07597b7ddc069cda061042fc381150a2e
carbonate <carbonatezero@gmail.com>
2026-03-17 16:38:18 -0400

    Implement softmax classifier and demo

---
+++ src/models/softmax.py
@@ -0,0 +1,110 @@
+from builtins import range
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+
+def softmax_loss_naive(W, X, y, reg):
+    """
+    Softmax loss function, naive implementation (with loops)
+
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    for i in range(num_train):
+        scores = X[i].dot(W)
+
+        # compute the probabilities in numerically stable way
+        scores -= np.max(scores)
+        p = np.exp(scores)
+        p /= p.sum()  # normalize
+        logp = np.log(p)
+
+        loss -= logp[y[i]]  # negative log probability is the loss
+        p[y[i]] -= 1
+        dW += np.outer(X[i], p)
+
+
+    # normalized hinge loss plus regularization
+    loss = loss / num_train + reg * np.sum(W * W)
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dW.                #
+    # Rather that first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the     #
+    # loss is being computed. As a result you may need to modify some of the    #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+    dW /= num_train
+    dW += 2 * reg * W
+
+    return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+    """
+    Softmax loss function, vectorized version.
+
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the softmax loss, storing the           #
+    # result in loss.                                                           #
+    #############################################################################
+    N = X.shape[0]
+
+    # scores: (N, C)
+    scores = X.dot(W)
+
+    # numerical stability
+    scores -= np.max(scores, axis=1, keepdims=True)
+
+    # probabilities: (N, C)
+    exp_scores = np.exp(scores)
+    p = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+    # loss
+    correct_class_probs = p[np.arange(N), y]
+    loss = -np.sum(np.log(correct_class_probs))
+    loss = loss / N + reg * np.sum(W * W)
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the gradient for the softmax            #
+    # loss, storing the result in dW.                                           #
+    #                                                                           #
+    # Hint: Instead of computing the gradient from scratch, it may be easier    #
+    # to reuse some of the intermediate values that you used to compute the     #
+    # loss.                                                                     #
+    #############################################################################
+    # gradient
+    p[np.arange(N), y] -= 1
+    dW = X.T.dot(p)
+    dW = dW / N + 2 * reg * W
+
+    return loss, dW
